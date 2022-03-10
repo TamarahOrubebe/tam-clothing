3ffc5b1e995b60f91f5083010854b2c3a74f2bb0
Your Name <you@example.com>
2022-03-10 03:22:54 +0100

    Update menu-items to navigate with HOC

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import './App.css';
-
 import HomePage from './pages/homepage/homepage.component';
 
+import { Switch, Route} from 'react-router-dom';
+
+const Hats = () => {
+  return <div>
+    <h1>Hats</h1>
+  </div>
+}
 
 function App() {
   return (
     <div >
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop/hats' component={Hats} />
+      </Switch>
     </div>
   );
 }
