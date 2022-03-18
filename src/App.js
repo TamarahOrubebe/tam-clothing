c879d0d3502d48465ee48a10aeeb2d82eb07f72d
Your Name <you@example.com>
2022-03-18 20:15:33 +0100

    implemented firebase utils, included the ability to store authenticated users into the firestore database

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUp from './pages/signin-and-signup-page/signin-and-signup-page.component';
 import { auth } from './firebase/firebase.utils';
+import { createUserProfileDocument } from './firebase/firebase.utils';
 
 
 
@@ -23,10 +24,30 @@ class App extends React.Component {
   unSubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unSubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user })
-      console.log(user);
-    
+    this.unSubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+
+      if (userAuth) {
+  
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot =>
+          
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          }, () => {
+            console.log(this.state);
+          })
+        )
+          console.log(this.state);
+        
+
+      } else {
+        this.setState({currentUser: null})
+      }
+      
     });
   
   }
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -14,8 +14,39 @@ const config = {
 
 firebase.initializeApp(config);
 
+export const createUserProfileDocument = async (userAuth, otherData) => {
+	if (!userAuth) return;
+
+	const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+	const snapShot = await userRef.get();
+
+	if (!snapShot.exists) {
+		const { displayName, email } = userAuth;
+
+		const createdAt = new Date();
+
+        try {
+
+           await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...otherData,
+            });
+            
+        } catch (error) {
+            console.log('error creating user profile', error.message);
+        }
+		
+	}
+
+	return userRef;
+};
+
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
+
 
 const provider = new firebase.auth.GoogleAuthProvider();
 
